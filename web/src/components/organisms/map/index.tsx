bc77eaa0e3afa7f208caf94c93ce85d61dce5fff
Kazuya Hara <iam@kazuyahara.dev>
2022-07-28 16:20:35 +0900

    feat(web): handle default position by browser language

--- web/src/components/organisms/map/index.tsx
+++ web/src/components/organisms/map/index.tsx
@@ -1,22 +1,28 @@
 import React, { memo } from 'react';
 
 import { GoogleMap, GoogleMapProps } from '@react-google-maps/api';
+import { useTranslation } from 'react-i18next';
 
-const defaultCenter = { lat: 35.6809591, lng: 139.7673068 };
 const defaultContainerStyle = { height: '100vh', width: '100vw' };
 const defautlZoom = 10;
 
 type Props = Pick<GoogleMapProps, 'center' | 'children' | 'mapContainerStyle' | 'zoom'>;
 
 function Map({
-  center = defaultCenter,
+  center,
   children,
   mapContainerStyle = defaultContainerStyle,
   zoom = defautlZoom,
 }: Props) {
+  const { i18n } = useTranslation();
+
+  const defaultCenter = i18n.language.startsWith('ja')
+    ? { lat: 35.6809591, lng: 139.7673068 }
+    : { lat: 41.892464, lng: 12.485325 };
+
   return (
     <GoogleMap
-      center={center}
+      center={center || defaultCenter}
       mapContainerStyle={mapContainerStyle}
       options={{ fullscreenControl: false, keyboardShortcuts: false, mapTypeControl: false }}
       zoom={zoom}
